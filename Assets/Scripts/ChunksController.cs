3b62923197dca28c1751cd9c47c88ac6fa03fecd
GOLDER303 <szymonstepien@interia.pl>
2023-08-24 16:52:04 +0200

    Add endless world

---
+++ Assets/Scripts/ChunksController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunksController : MonoBehaviour
+{
+    [SerializeField] private GameObject chunkPrefab;
+    [SerializeField] private int chunkSize = 15;
+    [SerializeField] private int loadDistance = 3;
+    [SerializeField] private Transform playerTransform;
+
+    private Transform[] chunksGameObjects;
+    private Vector3 prevPlayerPosition;
+
+    private void Start()
+    {
+        chunksGameObjects = new Transform[(loadDistance * 2 + 1) * (loadDistance * 2 + 1)];
+        prevPlayerPosition = playerTransform.position;
+
+        SpawnInitialChunks();
+    }
+
+    private void Update()
+    {
+
+        if (playerTransform.position != prevPlayerPosition)
+        {
+            UpdateChunks();
+            prevPlayerPosition = playerTransform.position;
+        }
+    }
+
+    private void SpawnInitialChunks()
+    {
+        GameObject chunksParent = new GameObject("Chunks");
+
+        int i = 0;
+        for (int xOffset = -loadDistance; xOffset <= loadDistance; xOffset++)
+        {
+            for (int yOffset = -loadDistance; yOffset <= loadDistance; yOffset++)
+            {
+                Vector2 chunkCoordinates = new Vector2(xOffset, yOffset);
+
+                GameObject chunkGameObject = Instantiate(chunkPrefab, chunkCoordinates * chunkSize, Quaternion.identity, chunksParent.transform);
+
+                chunksGameObjects[i] = chunkGameObject.transform;
+                i++;
+            }
+        }
+    }
+
+    private void UpdateChunks()
+    {
+        foreach (Transform chunk in chunksGameObjects)
+        {
+            Vector2 distanceFromPlayer = chunk.position - playerTransform.position;
+
+            if (Mathf.Abs(distanceFromPlayer.x) > chunkSize * (loadDistance + 1))
+            {
+                if (playerTransform.position.x < prevPlayerPosition.x)
+                {
+                    chunk.transform.position -= new Vector3((loadDistance * 2 + 1) * chunkSize, 0f, 0f);
+                }
+                else if (playerTransform.position.x > prevPlayerPosition.x)
+                {
+                    chunk.transform.position += new Vector3((loadDistance * 2 + 1) * chunkSize, 0f, 0f);
+                }
+            }
+            else if (Mathf.Abs(distanceFromPlayer.y) > chunkSize * (loadDistance + 1))
+            {
+                if (playerTransform.position.y < prevPlayerPosition.y)
+                {
+                    chunk.transform.position -= new Vector3(0f, (loadDistance * 2 + 1) * chunkSize, 0f);
+                }
+                else if (playerTransform.position.y > prevPlayerPosition.y)
+                {
+                    chunk.transform.position += new Vector3(0f, (loadDistance * 2 + 1) * chunkSize, 0f);
+                }
+            }
+        }
+    }
+}
