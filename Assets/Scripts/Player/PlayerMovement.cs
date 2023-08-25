3073e8e66a96806e5adca6023156dd9d0b43980f
GOLDER303 <szymonstepien@interia.pl>
2023-08-25 17:20:19 +0200

    Make player unable to move past ocean floor

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private GameManager gameManager;
 
     private Vector2 moveVector;
     public Vector2 facingDirection { get; private set; } = Vector2.right;
@@ -19,7 +20,15 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        playerRigidBody.AddForce(moveSpeed * Time.deltaTime * moveVector);
+        if (!(-transform.position.y > gameManager.MaxDepth))
+        {
+            playerRigidBody.AddForce(moveSpeed * Time.deltaTime * moveVector);
+        }
+
+        if (-transform.position.y > gameManager.MaxDepth)
+        {
+            transform.position = new Vector3(transform.position.x, -(gameManager.MaxDepth - 1), transform.position.z);
+        }
     }
 
     private void OnMove(InputValue inputValue)
