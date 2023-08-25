47952d8034dc8a325d071b09e399b6e3c926f3e8
GOLDER303 <szymonstepien@interia.pl>
2023-08-25 15:35:04 +0200

    Spawn different enemies depending on current depth

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -4,30 +4,46 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-    [SerializeField] private Enemy enemyPrefab;
+    [SerializeField] private Enemy[] enemyPrefabs;
     [SerializeField] private GameObject playerGameObject;
     [SerializeField] private float minSpawnDelay = 4f;
     [SerializeField] private float maxSpawnDelay = 1f;
     [SerializeField] private float spawnDistance = 15f;
     [SerializeField] private DifficultyManager difficultyManager;
+    [SerializeField] private int amountOfEnemyTypePerDepthLevel = 2;
 
     private void Start()
     {
-        StartCoroutine(SpawnEnemy(enemyPrefab));
+        StartCoroutine(SpawnEnemy());
     }
 
-    private IEnumerator SpawnEnemy(Enemy enemy)
+    private IEnumerator SpawnEnemy()
     {
         while (true)
         {
             Vector2 spawnPointDirection = Random.insideUnitCircle.normalized;
             Vector2 spawnPointCoordinates = spawnPointDirection * spawnDistance + new Vector2(transform.position.x, transform.position.y);
 
-            Enemy spawnedEnemy = Instantiate(enemy, spawnPointCoordinates, Quaternion.identity);
+            Enemy enemyPrefab = GetEnemyPrefab();
+
+            Enemy spawnedEnemy = Instantiate(enemyPrefab, spawnPointCoordinates, Quaternion.identity);
 
             spawnedEnemy.targetGameObject = playerGameObject;
 
             yield return new WaitForSeconds(minSpawnDelay + difficultyManager.CurrentDepthPercentage * (maxSpawnDelay - minSpawnDelay));
         }
+    }
+
+    private Enemy GetEnemyPrefab()
+    {
+        int minResourcePrefabIndex = (int)Mathf.Floor(enemyPrefabs.GetLength(0) * difficultyManager.CurrentDepthPercentage);
+        int maxResourcePrefabIndex = minResourcePrefabIndex + amountOfEnemyTypePerDepthLevel;
+
+        if (maxResourcePrefabIndex > enemyPrefabs.GetLength(0))
+        {
+            maxResourcePrefabIndex = enemyPrefabs.GetLength(0);
+        }
+
+        return enemyPrefabs[Random.Range(minResourcePrefabIndex, maxResourcePrefabIndex)];
     }
 }
